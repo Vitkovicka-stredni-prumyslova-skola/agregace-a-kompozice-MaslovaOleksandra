1de414ea4c5a95ac054d4150e4a1fab540c39c71
MaslovaOleksandra <159123855+MaslovaOleksandra@users.noreply.github.com>
2026-02-02 12:25:37 +0100

    3

--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ab34eef3fcf6546ef454a018c366d9fc2bf5ce9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ac7c4f106adefb9aec7a15d14f78cc776b2cd49")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
